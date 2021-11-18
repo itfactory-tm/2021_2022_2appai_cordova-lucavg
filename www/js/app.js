0fe037589df2896a5abfe19a90822a06b6344cdf
Luca Van Genechten <r0849476@student.thomasmore.be>
2021-11-18 17:07:03 +0100

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -32,7 +32,7 @@ initializeApp({
 	databaseURL: 'https://ledenlijst-chiro.firebaseapp.com'
 });
 
-import serviceAccount from './serviceAccount'
+import serviceAccount from './serviceAccount.json'
 /*const serviceAccount = {};
 fs.readFile('./serviceAccount.json', 'utf-8', (err, data) => {
   if (err) throw err
