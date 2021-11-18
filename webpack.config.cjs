34ced0340f6fc3d7799651f3c48729dccf4e410f
Luca Van Genechten <r0849476@student.thomasmore.be>
2021-11-18 22:51:00 +0100

    big bugfixes

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,5 +1,4 @@
-const path = require('path');
-//const HtmlWebpackPlugin = require('html-webpack-plugin');
+import path from 'path';
 
 module.exports = {
     mode: 'development',
--- www/js/app.js
+++ www/js/app.js
@@ -1,5 +1,35 @@
+import {
+	initializeApp,
+	applicationDefault,
+	cert
+} from '../../node_modules/firebase-admin/lib/app/index.js';
+import {
+	getFirestore,
+	Timestamp,
+	FieldValue
+} from '../../node_modules/firebase-admin/lib/firestore/index.js';
+
+import $ from './jquery';
+import serviceAccount from './serviceAccount.json'
+
+console.log('Device is ready');
+
 function onDeviceReady() {
-    console.log('Device is ready');
+	initializeApp({
+		credential: cert(serviceAccount)
+	});
+
+	const db = getFirestore();
+
+	const docRef = db.collection('Members').doc('member3');
+
+	(async () => {
+		const snapshot = await db.collection('Members').get();
+		snapshot.forEach((doc) => {
+			console.log(doc.id, '=>', doc.data());
+		});
+	});
+
 };
 
 $(function () {
